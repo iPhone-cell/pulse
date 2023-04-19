b30aada7a0d43743c0d57d8414cf8c354776c024
Илья <ilvlagad@gmail.com>
2023-04-20 00:14:05 +0300

    Создана карусель

--- js/script.js
+++ js/script.js
@@ -18,3 +18,20 @@ window.addEventListener('DOMContentLoaded', () => {
 		})
 	})
 })
+
+$(document).ready(function () {
+	$('.carousel__inner').slick({
+		speed: 1200,
+		prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow-prev.png"></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow-next.png"></button>',
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					dots: true,
+					arrows: false
+				}
+			}
+		]
+	});
+});
